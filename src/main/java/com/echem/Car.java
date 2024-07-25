d24dbbe4b134d3bdfc99d5591fe371a91c08a032
dmytroafr <afrosin.dmytro@gmail.com>
2024-07-25 21:25:42 +0300

    upload3

--- src/main/java/com/echem/Car.java
+++ src/main/java/com/echem/Car.java
@@ -3,21 +3,25 @@ package com.echem;
 public class Car implements Machine{
     @Override
     public String drive() {
+        System.out.println("car drive");
         return "car drive";
     }
 
     @Override
     public String stop() {
+        System.out.println("car stop");
         return "car stop";
     }
 
     @Override
     public String turnLeft() {
+        System.out.println("car turn left");
         return "car turn left";
     }
 
     @Override
     public String turnRight() {
+        System.out.println("car turn right");
         return "car turn right";
     }
 }
